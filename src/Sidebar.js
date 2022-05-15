9f0ccec19da7ebd2d692b8d908480662366f2ed9
DANYELKAR <danyelkar1905@gmail.com>
2022-05-15 17:25:12 +0300

    Sidebar UI is done

---
+++ src/Sidebar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import SidebarOptions from "./SidebarOptions";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListIcon from "@mui/icons-material/List";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Button } from "@mui/material";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <TwitterIcon />
+      <SidebarOptions active Icon={HomeIcon} text="Home" />
+      <SidebarOptions Icon={SearchIcon} text="Explore" />
+      <SidebarOptions Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOptions Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOptions Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOptions Icon={ListIcon} text="Lists" />
+      <SidebarOptions Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOptions Icon={MoreHorizIcon} text="More" />
+      <Button variant="outlined" className="sidebar-tweet" fullWidth>
+        TWEET
+      </Button>
+    </div>
+  );
+};
+
+export default Sidebar;
